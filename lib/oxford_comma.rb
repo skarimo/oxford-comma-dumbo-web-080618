0339dba22e98298cfa9b6145cd3398436e4d2240
Sherzod Karimov <karimovj@gmail.com>
2018-07-20 10:04:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ new_arr = []
   if arr.length == 1 
     new_arr << arr[0] 
   elsif arr.length == 2 
-    new_arr << arr.join("and ")
+    new_arr << arr.join(" and ")
   else 
-    arr[-1] = " and " + arr[-1]
+    arr[-1] = "and " + arr[-1]
     new_arr << arr.join(", ")
   end 
   new_arr.join
