c382b18dd060b8bb64a6f8aadf5c92792fc00524
Sherzod Karimov <karimovj@gmail.com>
2018-07-20 10:04:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+new_arr = []
+  if arr.length == 1 
+    new_arr << arr[0] 
+  elsif arr.length == 2 
+    new_arr << arr.join(" and ")
+  else 
+    arr[-1] = "and " + arr[-1]
+    new_arr << arr.join(", ")
+  end 
+  new_arr.join
 end
